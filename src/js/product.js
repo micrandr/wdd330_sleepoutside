25529f30eaacdb0d4fa5c63e874274a92ac3ff29
Mickael <micrandr@gmail.com>
2024-10-09 17:59:38 +0300

    week02 - team activity

--- src/js/product.js
+++ src/js/product.js
@@ -1,22 +1,30 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
-import { findProductById } from "./ProductData.mjs";
+import { setLocalStorage, getLocalStorage, getParam } from './utils.mjs';
+import ProductData from './ProductData.mjs';
+import ProductDetails from './ProductDetails.mjs';
 
-  //const dataSource = new ProductData("tents");
+
+const dataSource = new ProductData("tents");
+const productId = getParam("product");
+
+const product = new ProductDetails(productId, dataSource);
+product.init();
+
+  // //const dataSource = new ProductData("tents");
   
-  function addProductToCart(product) {
-    // TODO product should be an array
-    const currentCart = getLocalStorage("so-cart") || [];
-    currentCart.push(product);
-    setLocalStorage("so-cart", currentCart);
-  }
+  // function addProductToCart(product) {
+  //   // TODO product should be an array
+  //   const currentCart = getLocalStorage("so-cart") || [];
+  //   currentCart.push(product);
+  //   setLocalStorage("so-cart", currentCart);
+  // }
   
-  // add to cart button event handler
-  async function addToCartHandler(e) {  
-    const product = await findProductById(e.target.dataset.id);
-    addProductToCart(product);
-  }
+  // // add to cart button event handler
+  // async function addToCartHandler(e) {  
+  //   const product = await findProductById(e.target.dataset.id);
+  //   addProductToCart(product);
+  // }
   
-  // add listener to Add to Cart button
-  document
-    .getElementById("addToCart")
-    .addEventListener("click", addToCartHandler);
+  // // add listener to Add to Cart button
+  // document
+  //   .getElementById("addToCart")
+  //   .addEventListener("click", addToCartHandler);
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -33,24 +33,24 @@ export function renderListWithTemplate(
   templateFn,
   parentElement,
   list,
-  position = "afterbegin",
+  position = 'afterbegin',
   clear = false
 ) {
   const htmlStrings = list.map(templateFn);
   // if clear is true we need to clear out the contents of the parent.
   if (clear) {
-    parentElement.innerHTML = "";
+    parentElement.innerHTML = '';
   }
-  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(''));
 }
 
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
-  qs(selector).addEventListener("touchend", (event) => {
+  qs(selector).addEventListener('touchend', (event) => {
     event.preventDefault();
     callback();
   });
-  qs(selector).addEventListener("click", callback);
+  qs(selector).addEventListener('click', callback);
 }
 
 async function loadTemplate(path) {
@@ -61,17 +61,17 @@ async function loadTemplate(path) {
 
 // dynamically load header and footer into page
 export async function loadHeaderFooter() {
-  const headerTemplate = await loadTemplate("../public/partials/header.html");
-  const headerElement = document.querySelector("#main-header");
-  const footerTemplate = await loadTemplate("../public/partials/footer.html");
-  const footerElement = document.querySelector("#main-footer");
+  const headerTemplate = await loadTemplate('/partials/header.html');
+  const headerElement = document.querySelector('#main-header');
+  const footerTemplate = await loadTemplate('/partials/footer.html');
+  const footerElement = document.querySelector('#main-footer');
 
   renderWithTemplate(headerTemplate, headerElement);
   renderWithTemplate(footerTemplate, footerElement);
 }
 
 export function renderWithTemplate(template, parentElement, data, callback) {
-  parentElement.insertAdjacentHTML("afterbegin", template);
+  parentElement.insertAdjacentHTML('afterbegin', template);
   //if callback... call it and pass data
   if (callback) {
     callback(data);
